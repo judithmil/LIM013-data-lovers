3b2d220466bcf28df69da4642c457a57dce000fe
Elizabeth Rivera <elizabethriver@gmail.com>
2020-08-29 12:59:03 -0500

    actualizacion de archivos html css y js de la vista 3 e incorporacion de la vista 2

--- src/main.js
+++ src/main.js
@@ -1,109 +1,107 @@
-import data from "./data/lol/lol.js";
-import { filterData, sortData, searchDataFunction } from "./data.js";
+import order from './data.js';
+import data from './data/lol/lol.js';
 
-/*--cargar la data--*/
-const objData = data.data;
-const arrayData = Object.values(objData);
-containerOfAllFunctions(arrayData);
+const datos = data.data,
+  filtradoCampeones = document.getElementById("filtradoCampeones"),
+  arrayCampeones = Object.values(datos);
 
-const sortKind = document.getElementById("options");
-const inputSearch = document.getElementById("searchInputs");
-const searchbtton = document.getElementById("lupa");
-const arrayTag = Object.values(document.getElementsByClassName("fil-champ"));
 
-function containerOfAllFunctions(arrayChamp) {
-  const containerChamp = document.getElementById("container-listchamp");
-  const cardChamp = (arrayChamp) => {
-    let newArrayChamp = [];
-    let contend = [];
-    containerChamp.value = "";
-    for (let index = 0; index < arrayChamp.length; index++) {
-      newArrayChamp.push(Object.assign({}, arrayChamp[index]));
+const listaCampeones = (name, img, title, difficulty) => {
+  const campeones = document.createElement("div"),
+    splashCampeones = document.createElement("img"),
+    nameCampeones = document.createElement("div"),
+    titleCampeones = document.createElement("div"),
+    difficultyCampeones = document.createElement("div");
+
+    nameCampeones.innerHTML += name;
+    titleCampeones.innerHTML += `<p>"${title}"</p>`;
+    difficultyCampeones.innerHTML += `<p>Dificultad: "${difficulty}"</p>`;
+  campeones.setAttribute("class", "legends");
+  splashCampeones.setAttribute("class", "img-container");
+  splashCampeones.setAttribute("src", img);
+  nameCampeones.setAttribute("class", "name");
+  titleCampeones.setAttribute("class", "name");
+  difficultyCampeones.setAttribute("class", "difficulty");
+
+  document.getElementById("container-campeones").appendChild(campeones);
+  campeones.appendChild(splashCampeones);
+  campeones.appendChild(nameCampeones);
+  campeones.appendChild(titleCampeones);
+  campeones.appendChild(difficultyCampeones);
+};
+
+/*---TRAER DATA---*/
+const getCampeones = (objCampeones) => {
+  for (let i = 0; i < objCampeones.length; i++) {
+    let name = objCampeones[i].name;
+    let img = objCampeones[i].splash;
+    let title = objCampeones[i].title;
+    let difficulty = objCampeones[i].info.difficulty;
+    listaCampeones(name, img, title, difficulty);
+  }
+};
+
+getCampeones(arrayCampeones);
+
+
+filtradoCampeones.addEventListener('click', (e) => {
+  const rol = e.target.id
+
+  if (rol == null || rol == '' || rol == 'All') {
+    getCampeones(arrayCampeones);
+  } else {
+    const result = order.filterChampions(arrayCampeones, rol)
+    document.getElementById('container-campeones').innerHTML = '';
+    getCampeones(result)
+  }
+})
+
+/*ORDER */
+const selector = document.querySelector("#order");
+selector.addEventListener("click", (event) => {
+  const orderName = event.target.value;
+  //console.log("prueba",e.target.value);
+  if (orderName == "asc") {
+    const prueba = order.orderAZ(arrayCampeones);
+    document.getElementById("container-campeones").innerHTML = "";
+    getCampeones(prueba)
+  } else if (orderName == "desc") {
+    const prueba = order.orderZA(arrayCampeones);
+    document.getElementById("container-campeones").innerHTML = "";
+    getCampeones(prueba)
+  }
+});
+
+
+const searchInput = document.querySelector('#searchInput'),
+  result = document.querySelector('#container-campeones');
+
+const buscador = () => {
+
+  result.innerHTML = '';
+  const texto = searchInput.value.toLowerCase();
+
+  for (let campeones of arrayCampeones) {
+    let nombre = campeones.name.toLowerCase()
+    if (nombre.indexOf(texto) != -1) {
+
+      result.innerHTML +=
+        `<div class="legends">
+        <img class="img-container" src="${campeones.splash}" alt="">
+        <div class="name">${campeones.name}</div>
+        <div class="name">"${campeones.title}"</div>
+        <div class="difficulty">"${campeones.info.difficulty}"</div>
+      </div>`
     }
-    for (let index = 0; index < newArrayChamp.length; index++) {
-      contend.push(`
-      <div class ="card-link">
-        <a class="blog-card" id="${
-          newArrayChamp[index].id
-        }" href="#openmodal${index}">
-      <div>
-        <img class="post-image " src="${newArrayChamp[index].splash}" />
-        <div class="article-details" >
-          <h1 class="post-name" id="${newArrayChamp[index].id}">${
-        newArrayChamp[index].name
-      } </h1>
-          <h3 class="post-title" id="${newArrayChamp[index].id}"> ${
-        newArrayChamp[index].title
-      }</h3>
-          <h3 class="post-title" id="${newArrayChamp[index].id}"> ${
-        newArrayChamp[index].info.difficulty
-      }</h3>
-        </div>                   
-      </div>
-      </a>
-      </div>
+  }
 
-      <section id="openmodal${index}" class="modal-window">
-        <div class = "modal-content" id="${newArrayChamp[index.id]}">
-        <a href="#" title="Close" class="modal-close">X</a>
-        <img class="modal-img" src="${newArrayChamp[index].splash}" />
-        <div class="modal-info">
-        <img src="${newArrayChamp[index].img}"/>
-        <h1>${newArrayChamp[index].name}</h1>
-        <h3>${newArrayChamp[index].title}</h3>
-        <p>${newArrayChamp[index].blurb}</p>
-        <p>${newArrayChamp[index].tags}</p>
-        <table>
-        <tr>
-          <th> Health Points (HP)</th>
-          <th> HP per Level</th>
-          <th> HP at Lvl. 5</th>
-          <th> HP at Lvl. 10</th>
-          <th> HP at Lvl. 15</th>
-          <th> HP at Lvl. 18</th>
-        </tr>
-        <tr>
-          <td>${newArrayChamp[index].stats.hp}</td>
-          <td>${newArrayChamp[index].stats.hpperlevel}</td>
-        </tr>
-        </table>
-        </div>
-      </div>
-      </section>`);
-    }
-    containerChamp.innerHTML = contend.join("");
-  };
-
-  cardChamp(arrayChamp);
-  /*TODO mejorar funcion */
-  const listChamp = (arrayRol) => {
-    let arrayListChamp = [];
-    arrayRol.forEach((tags) => {
-      tags.addEventListener("change", () => {
-        if (tags.checked === true) {
-          arrayListChamp.push(tags.value);
-        } else {
-          const x = arrayListChamp.indexOf(tags.value);
-          arrayListChamp.splice(x, 1);
-          cardChamp(arrayChamp);
-        }
-        cardChamp(filterData(arrayChamp, arrayListChamp));
-      });
-    });
-  };
-  listChamp(arrayTag);
-  const search = () => {
-    const listBySort = sortKind.options[sortKind.selectedIndex].value;
-    const arraySearch = searchDataFunction(arrayChamp, inputSearch.value);
-    cardChamp(
-      sortData(arraySearch, parseInt(listBySort[0]), parseInt(listBySort[1]))
-    );
-  };
-
-  const searchRolandOrder = () => {
-    search();
-    sortKind.addEventListener("change", search);
-    searchbtton.addEventListener("click", search);
-  };
-  searchRolandOrder();
+  if (result.innerHTML == '') {
+    result.innerHTML +=
+      `<div class="legends">
+      <img class="img-container" src="./imagenes/notFound.gif" alt="">
+      <div class="name">Not Found</div>
+    </div>`
+  }
 }
+
+searchInput.addEventListener('keyup', buscador)
